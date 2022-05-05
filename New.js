f9d179298ca57672b5ca38efa5b3c62c28121def
Iftekhar Qureshi <62373287+ifte100@users.noreply.github.com>
2022-05-05 20:13:21 +1000

    Add files via upload

--- New.js
+++ New.js
@@ -4,20 +4,26 @@ function addToCart(item) {
     shoppingCart.push(item);
 }
 
-function removeFromCart(item) {
+function removeFromCart(itemName) {
     //searching for the item in the cart using .includes()
-    if(shoppingCart.includes(item)) {
-        const index = shoppingCart.indexOf(item);
-        if( index > -1 ) {
-            shoppingCart.splice(index, 1);
-        }
-        else{
-            console.log("No item in the cart!");
-        }
+    for(var i = 0; i < shoppingCart.length; i++) {
+     if(shoppingCart[i].name === itemName) {
+             shoppingCart[i].quantity--;
+            if(shoppingCart[i].quantity === 0) {
+                shoppingCart.splice(shoppingCart[i], 1);
+            }
+            totalCost();
+            break;
+         }
+     }
+}
+
+function removeAllFromCart() {
+    while(shoppingCart.length > 0)
+    {
+        shoppingCart.pop();
     }
-    else{
-        console.log("No such item");
-    }
+    totalCost();
 }
 
 function totalCost() {
@@ -33,18 +39,21 @@ function totalCost() {
     return Math.round(total * 100) / 100;
 }
 
-// var newItem = {name: "Apple", price: 4.95, quantity: 2};
-// //var secondItem = {name: Apple, price: 4.95, quantity: 1};
-// var thirdItem = {name: "Orange", price: 3.99, quantity: 1};
+var newItem = {name: "Apple", price: 4.95, quantity: 2};
+var thirdItem = {name: "Orange", price: 3.99, quantity: 1};
 
 // addToCart(newItem);
-// //addToCart(secondItem);
 // addToCart(thirdItem);
+// console.log(shoppingCart);
+// removeAllFromCart();
+// console.log(shoppingCart);
+// removeFromCart("Apple");
 // console.log(shoppingCart);
 // console.log(totalCost());
 
 export {
     addToCart,
     removeFromCart,
+    removeAllFromCart,
     totalCost
 };
